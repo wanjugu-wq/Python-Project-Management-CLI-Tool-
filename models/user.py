0867655f99bf0c62c41c353ceab889f4a3fea897
wanjugu-wq <wanjuguzzz@gmail.com>
2026-06-11 21:47:55 +0300

    fixed user id bug w json persistence

--- models/user.py
+++ models/user.py
@@ -1,8 +1,14 @@
 from models.person import Person
 from models.project import Project
+import json
 
 class User(Person):
-    user_count = 0
+    try:
+        with open("data/database.json", "r") as file:
+            data = json.load(file)
+            user_count = len(data.get("users", []))
+    except (FileNotFoundError, json.JSONDecodeError):
+        user_count = 0
 
     def __init__(self, name, email):
         super().__init__(name)
