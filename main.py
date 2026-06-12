8d593bdab432b68370de700295dd7db2585ff074
wanjugu-wq <wanjuguzzz@gmail.com>
2026-06-12 11:47:12 +0300

    fixed bugs and added CLI error handlers

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ add_user.add_argument("--email", required=True)
 add_project = subparsers.add_parser("add-project")
 add_project.add_argument("--user-id", type=int, required=True)
 add_project.add_argument("--title", required=True)
-add_project.add_argument("--description", required=True)
+add_project.add_argument("--description", required=False)
 
 list_projects = subparsers.add_parser("list-projects")
 list_projects.add_argument("--user-id", type=int, required=True)
@@ -44,27 +44,39 @@ args = parser.parse_args()
 if args.command == "add-user":
     data = load_data()
 
-    user = User(args.name, args.email)
+    try:
+        user = User(args.name, args.email)
 
-    data["users"].append(user.to_dict())
+        data["users"].append(user.to_dict())
 
-    save_data(data)
+        save_data(data)
 
-    print(f"User created: {user}")
+        print(f"User created: {user}")
+    except ValueError as e:
+        print(f"Error :{e}")
 elif args.command == "list-users":
     data = load_data()
 
-    for user in data["users"]:
-        print(
-            f"User {user['id']}: "
-            f"{user['name']} "
-            f"({user['email']})"
-        )
+    try:
+        for user in data["users"]:
+            print(
+                f"User {user['id']}: "
+                f"{user['name']} "
+                f"({user['email']})"
+            )
+        if len(data["users"]) == 0:
+            print("No users created")
+    except Exception as e:
+        print(f"Error :{e}")
 elif args.command == "add-project":
     data = load_data()
+
+    user_found = False
+
     try:
         for user in data["users"]:
             if user["id"] == args.user_id:
+                user_found = True
 
                 project = {
                 "title": args.title,
@@ -76,68 +88,126 @@ elif args.command == "add-project":
 
                 save_data(data)
 
-                print("Project added successfully.")
+                print("Project added successfully!")
                 break
-    except ValueError as e:
-        print(f"Error creating user: {e}")
+        if not user_found:
+            print("User not found")
+    except Exception as e:
+        print(f"Error :{e}")
 elif args.command == "list-projects":
     data = load_data()
+    user_found = False
 
-    for user in data["users"]:
-        if user["id"] == args.user_id:
+    try:
+        for user in data["users"]:
+            if user["id"] == args.user_id:
+                user_found = True
 
-            for project in user["projects"]:
-                print(f"Project: {project['title']}")
-                print(f"Description: {project['description']}")
-                print()
+                for project in user["projects"]:
+                    print(f"Project: {project['title']}")
+                    print(f"Description: {project['description']}")
+                    print()
+                if len(user["projects"]) == 0:
+                    print("No projects created")
+        if not user_found:
+            print("User not found")
+    except Exception as e:
+        print(f"Error :{e}")
 elif args.command == "add-task":
     data = load_data()
 
-    for user in data["users"]:
-        if user["id"] == args.user_id:
+    user_found = False
+    project_found = False
 
-            for project in user["projects"]:
-                if project["title"] == args.project:
+    try:
+        for user in data["users"]:
+            if user["id"] == args.user_id:
+                user_found = True
 
-                    task = {
-                        "id": len(project["tasks"]) + 1,
-                        "title": args.title,
-                        "status": "Pending"
-                    }
+                for project in user["projects"]:
+                    if project["title"] == args.project:
+                        project_found = True
 
-                    project["tasks"].append(task)
+                        task = {
+                            "id": len(project["tasks"]) + 1,
+                            "title": args.title,
+                            "status": "Pending"
+                        }
 
-                    save_data(data)
+                        project["tasks"].append(task)
 
-                    print("Task added successfully.")
-                    break
+                        save_data(data)
+
+                        print("Task added successfully.")
+                        break
+        if not user_found:
+            print("User not found")
+        if not project_found:
+            print("Project not found")
+        
+    except Exception as e:
+        print(f"Error :{e}")
 elif args.command == "list-tasks":
     data = load_data()
 
-    for user in data["users"]:
-        if user["id"] == args.user_id:
+    user_found = False
+    project_found = False
 
-            for project in user["projects"]:
-                if project["title"] == args.project:
+    try:
+        for user in data["users"]:
+            if user["id"] == args.user_id:
+                user_found = True
+                for project in user["projects"]:
 
-                    for task in project["tasks"]:
-                        status_color = "green" if task["status"] == "Completed" else "yellow"
+                    if project["title"] == args.project:
+                        project_found = True
 
-                        console.print(f"[{status_color}]Task {task['id']} - {task['title']}[/]"
-)
+                        for task in project["tasks"]:
+                            status_color = "green" if task["status"] == "Completed" else "yellow"
+
+                            console.print(f"[{status_color}]Task {task['id']} - {task['title']}[/]"
+                            )
+                        if len(project["tasks"]) == 0:
+                            print("No tasks created")
+                        break
+        if not user_found:
+            print("User not found")
+        if not project_found:
+            print("Project not found")
+
+    except Exception as e:
+       print(f"Error :{e}")
 elif args.command == "complete-task":
     data = load_data()
 
-    for user in data["users"]:
-        if user["id"] == args.user_id:
+    user_found = False
+    project_found = False
+    task_found = False
 
-            for project in user["projects"]:
-                if project["title"] == args.project:
+    try:
+        for user in data["users"]:
+            if user["id"] == args.user_id:
+                user_found = True
 
-                    for task in project["tasks"]:
-                        if task["id"] == args.task_id:
-                            task["status"] = "Completed"
-                            save_data(data)
-                            print("Task marked as completed.")
-                            break
+                for project in user["projects"]:
+                    
 
+                    if project["title"] == args.project:
+                        project_found = True
+
+                        for task in project["tasks"]:
+                            if task["id"] == args.task_id:
+                                task_found = True
+
+                                task["status"] = "Completed"
+                                save_data(data)
+                                print("Task marked as completed.")
+                                break
+        if not user_found:
+            print("User not found")
+        if not project_found:
+            print("Project not found")
+        if not task_found:
+            print("Task not found")
+    except Exception as e:
+        print(f"Error :{e}")
